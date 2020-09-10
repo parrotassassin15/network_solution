4bcb7ee424197d65a908145c0a1b87e4abb6bcf7
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-09-10 13:43:07 +0000

    Update network.sh

--- network.sh
+++ network.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#this script is meant to install the firmware for wlan0 wifi card
 
 echo "PLZ MAKE SURE SECURE BOOT IS DISABLED IN YOUR BIOS"
 sleep 5
+#installs depencies
 sudo apt install bc module-assistant build-essential dkms
 sudo apt-get install --reinstall git dkms build-essential linux-headers-$(uname -r)
+#clones the realtek firmware
 sudo git clone https://github.com/tomaspinho/rtl8821ce.git
 cd rtl8821ce/
 sudo m-a prepare
