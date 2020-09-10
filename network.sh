58bc80bef29a702d45470ffc7732d3202deea176
Jok3rTwin2-DEV <54908247+Jok3rTwin2-DEV@users.noreply.github.com>
2020-09-10 13:41:08 +0000

    Update network.sh

--- network.sh
+++ network.sh
@@ -3,10 +3,11 @@
 
 echo "PLZ MAKE SURE SECURE BOOT IS DISABLED IN YOUR BIOS"
 sleep 5
+sudo apt install bc module-assistant build-essential dkms
+sudo apt-get install --reinstall git dkms build-essential linux-headers-$(uname -r)
 sudo git clone https://github.com/tomaspinho/rtl8821ce.git
 cd rtl8821ce/
-sudo apt install bc module-assistant build-essential dkms
 sudo m-a prepare
 sudo ./dkms-install.sh
-sudo apt-get install --reinstall git dkms build-essential linux-headers-$(uname -r)
-
+clear && echo "you may now reboot!"
+echo "hint: you may need to reboot a few times"
